1de62cc8dcf47f1be3239eadf930416e5795c49d
Gabriel I Feliz <gfeliz3@montgomerycollege.edu>
2018-07-02 16:59:50 -0400

    initial commit

---
+++ src/main/java/com/example/demo/TastyNastyService.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class TastyNastyService {
+
+    @Autowired
+    TastyRepository tasties;
+
+    @Autowired
+    NastyRepository nasties;
+
+    public String last5MinuteResult(Long id) {
+        String tastyOrNasty="";
+
+        ArrayList<Tasty> dishTastyVotes = (ArrayList) tasties
+                        .findAllByTheDish_IdAndVotedAtAfter(
+                                id, LocalDateTime.now().minusMinutes(5));
+        System.out.println("Tasty Count: " + dishTastyVotes.size());
+
+        ArrayList<Tasty> dishNastyVotes = (ArrayList) nasties
+                .findAllByTheDish_IdAndVotedAtAfter(
+                        id, LocalDateTime.now().minusMinutes(5));
+        System.out.println("Nasty Count: " + dishNastyVotes.size());
+
+        if(dishTastyVotes.size() > dishNastyVotes.size())
+            tastyOrNasty="tasty";
+        else if (dishTastyVotes.size() == dishNastyVotes.size())
+            tastyOrNasty="undefined";
+        else
+            tastyOrNasty="nasty";
+
+        System.out.println(tastyOrNasty);
+        return tastyOrNasty;
+    }
+}
